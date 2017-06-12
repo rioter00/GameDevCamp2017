fb99a0788d6ecc01c43ca0f6d9245b2ee8ebbc42
Nick <nickthwang@gmail.com>
2017-06-12 15:01:50 -0500

    Add files via upload

---
+++ projectileMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileMovement : MonoBehaviour {
+
+	public float hSpeed;
+	public GameObject shooter;
+	bool facingRight;
+
+
+	// Use this for initialization
+	void Start () {
+		shooter = GameObject.FindGameObjectWithTag ("Player");
+		facingRight = shooter.GetComponent<character>().facingRight;
+		print (facingRight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (facingRight) {
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x + hSpeed, gameObject.transform.position.y, gameObject.transform.position.y);
+		} else {
+			gameObject.transform.position = new Vector3 (gameObject.transform.position.x - hSpeed, gameObject.transform.position.y, gameObject.transform.position.y);
+		}
+	}
+}
