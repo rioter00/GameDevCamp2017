1bbebd6ca4f35982890c1fdc83473fc5966b17f0
Nick <nickthwang@gmail.com>
2017-06-12 23:44:33 -0500

    Create spawnPrefab.cs

--- spawnPrefab.cs
+++ spawnPrefab.cs
@@ -7,6 +7,7 @@ public class spawnPrefab : MonoBehaviour {
 	public GameObject prefabPrototype;
 	GameObject newInstance;
 	public Transform spawnPoint;
+	public KeyCode spawnbutton;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,7 @@ public class spawnPrefab : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (spawnbutton)) {
 			newInstance = Instantiate (prefabPrototype, spawnPoint.position, Quaternion.identity) as GameObject;
 			newInstance.transform.parent = gameObject.transform;
 		}
